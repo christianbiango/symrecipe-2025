53b5869f86c0aaf208bad95cec3d9a3c99aeeb33
christianbiango <biangojunior@gmail.com>
2025-05-28 12:25:24 +0200

    feat: add update ingredient form

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class IngredientController extends AbstractController
 {
@@ -57,5 +58,28 @@ class IngredientController extends AbstractController
         }
 
         return $this->render('pages/ingredient/new.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route('/ingredient/edition/{id}', name: 'ingredient.edit', methods: ['GET', 'POST'])]
+    public function edit(Ingredient $ingredient, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $ingredient = $form->getData();
+
+            $manager->persist($ingredient);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été modifié avec succès'
+            );
+
+            return $this->redirectToRoute('ingredient.index');
+        }
+
+        return $this->render('pages/ingredient/edit.html.twig', ["form" => $form->createView()]);
     }
 }
