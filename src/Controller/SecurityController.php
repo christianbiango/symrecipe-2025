f8eed83d18eb46ff5c3e1d9fedf567e8fc6a46e8
christianbiango <biangojunior@gmail.com>
2025-05-29 11:43:17 +0200

    feat: add register user feature

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegitrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -21,6 +25,43 @@ class SecurityController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('pages/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    #[Route('/inscription', name: 'security.registration', methods: ['GET', 'POST'])]
+    public function registration(Request $request, EntityManagerInterface $manager, AuthenticationUtils $authenticationUtils) : Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home.index');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
+        $form = $this->createForm(RegitrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $user = $form->getData();
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                "success",
+                "Votre compte a bien été créé"
+            );
+
+            return $this->redirectToRoute('security.login');
+        }
+        echo $error;
+        return $this->render('pages/security/registration.html.twig', [
+            'form' => $form->createView(),
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
