258839fdd1ba76ae6e64d58e552c9169c6e940a7
christianbiango <biangojunior@gmail.com>
2025-05-29 13:02:31 +0200

    feat: add update password feature

---
+++ src/Controller/UserController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegitrationType;
+use App\Form\UserPasswordType;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class UserController extends AbstractController
+{
+    #[Route('/utilisateur/edition/{id}', name: 'user.edit')]
+    public function edit(User $user, EntityManagerInterface $manager, Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        if(!$this->getUser())
+        {
+            return $this->redirectToRoute('security.login');
+        }
+
+        if($this->getUser() !== $user)
+        {
+            return $this->redirectToRoute('recipe.index');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            if($hasher->isPasswordValid($user, $form->getData()->getPlainPassword())) {
+                $user = $form->getData();
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    "success",
+                    "Votre compte a été modifié avec succès"
+                );
+            }else{
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect.'
+                );
+            }
+        }
+
+        return $this->render('pages/user/edit.html.twig',
+            ["form" => $form->createView()],
+        );
+    }
+
+    #[Route('/utilisateur/edition-mot-de-passe/{id}', name: 'user.edit.password', methods: ['GET', 'POST'])]
+    public function editPassword(User $user, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            if($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                //HACK: looks like prePersist only works after the first flush so we have to setPassword for now
+                //$user->setPlainPassword($form->getData()['newPassword']);
+                $user->setPassword(
+                    $hasher->hashPassword(
+                        $user,
+                        $form->getData()['newPassword']
+                    )
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    "success",
+                    "Le mot de passe a été mis à jour"
+                );
+
+                return $this->redirectToRoute("recipe.index");
+            }else{
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect.'
+                );
+            }
+        }
+
+        return $this->render('pages/user/edit_password.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -113,7 +113,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getPlainPassword(): string
+    public function getPlainPassword(): ?string
     {
         return $this->plainPassword;
     }
